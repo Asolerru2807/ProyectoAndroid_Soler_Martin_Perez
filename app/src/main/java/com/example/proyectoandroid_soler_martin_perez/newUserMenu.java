94658377c7d4379b8ac75410bb4ef7affaa81b25
AlesconS <0345asoler@e-itaca.es>
2022-02-21 22:10:51 +0100

    Añadida la creacion de usuarios nuevos (solo rol usuario, los administradores se gestionaran desde firebase)

---
+++ app/src/main/java/com/example/proyectoandroid_soler_martin_perez/newUserMenu.java
@@ -0,0 +1,106 @@
+package com.example.proyectoandroid_soler_martin_perez;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class newUserMenu extends AppCompatActivity {
+
+    private EditText mEditTextNombre;
+    private EditText mEditTextCorreo;
+    private EditText mEditTextPass;
+    private Button btnRegistrar;
+
+    private String nombre;
+    private String correo;
+    private String pass;
+    private String rol;
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDataBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_user_menu);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+
+        mEditTextNombre = (EditText) findViewById(R.id.editTextName);
+        mEditTextCorreo = (EditText) findViewById(R.id.editTextCorreo);
+        mEditTextPass = (EditText) findViewById(R.id.editTextPass);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                nombre = mEditTextNombre.getText().toString();
+                correo = mEditTextCorreo.getText().toString();
+                pass = mEditTextPass.getText().toString();
+                rol = "usuario";
+
+                if(!nombre.isEmpty() && !correo.isEmpty() && !pass.isEmpty()){
+                    if(pass.length() >= 6){
+                        RegistrarUsuario();
+
+                    }else{
+                        Toast.makeText(newUserMenu.this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(newUserMenu.this, "No puede haber campos vacios!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void RegistrarUsuario(){
+        mAuth.createUserWithEmailAndPassword(correo, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task){
+                if(task.isSuccessful()){
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("nombre", nombre);
+                    map.put("correo", correo);
+                    map.put("pass", pass);
+                    map.put("rol", rol);
+
+                    String id = mAuth.getCurrentUser().getUid();
+
+                    mDataBase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if(task2.isSuccessful()){
+                                startActivity(new Intent(newUserMenu.this, usuario_creado.class));
+                                finish();
+                            }else{
+                                Toast.makeText(newUserMenu.this, "No se pudieron crear los datos correctamente", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(newUserMenu.this, "No se pudo registrar este usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/proyectoandroid_soler_martin_perez/usuario_creado.java
+++ app/src/main/java/com/example/proyectoandroid_soler_martin_perez/usuario_creado.java
@@ -3,13 +3,12 @@ package com.example.proyectoandroid_soler_martin_perez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class usuario_creado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_usuario_creado);
     }
 }
