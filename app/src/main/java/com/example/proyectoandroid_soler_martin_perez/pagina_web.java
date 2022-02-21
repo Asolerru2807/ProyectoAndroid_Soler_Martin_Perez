aa2d63923993a2e6d11d9294a950b1bc1b1cc176
miguel <0301mmartin@e-itaca.es>
2022-02-21 19:09:20 +0100

    Login y webview

---
+++ app/src/main/java/com/example/proyectoandroid_soler_martin_perez/pagina_web.java
@@ -0,0 +1,25 @@
+package com.example.proyectoandroid_soler_martin_perez;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class pagina_web extends AppCompatActivity {
+
+    WebView miVisorWeb;
+    String url = "https://www.e-itaca.es/index.php/avisos";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina_web);
+        miVisorWeb = (WebView) findViewById(R.id.pagina);
+        final WebSettings ajustesVisorWeb = miVisorWeb.getSettings();
+        ajustesVisorWeb.setJavaScriptEnabled(true);
+        miVisorWeb.loadUrl(url);
+
+    }
+}
