ed09d75f21f102e370b623607e34857fff7ca0cb
Mpgcole <mpgcole@gmail.com>
2022-02-22 20:48:42 +0100

    Menu Layout Mejorado

--- app/src/main/java/com/example/proyectoandroid_soler_martin_perez/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid_soler_martin_perez/MainActivity.java
@@ -11,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button miUsuario = (Button) findViewById(R.id.botonUsuario);
+
+        Button tienda = (Button) findViewById(R.id.botonTiendas);
+
+        Button consumo = (Button) findViewById(R.id.botonConsumo);
+
+        Button web = (Button) findViewById(R.id.botonWeb);
+
+        Button sugerencias = (Button) findViewById(R.id.botonSugerencias);
+
+        Button cerrarSesion = (Button) findViewById(R.id.botonCerrarSesion);
     }
 }
