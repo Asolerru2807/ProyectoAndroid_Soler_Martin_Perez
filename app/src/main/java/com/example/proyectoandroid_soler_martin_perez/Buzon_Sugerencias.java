8c6d6dce50b9a341233012ebf52d784e96ae79a7
miguel <0301mmartin@e-itaca.es>
2022-02-22 18:11:18 +0100

    cambio inicio de sesion

--- app/src/main/java/com/example/proyectoandroid_soler_martin_perez/Buzon_Sugerencias.java
+++ app/src/main/java/com/example/proyectoandroid_soler_martin_perez/Buzon_Sugerencias.java
@@ -1,6 +1,9 @@
 package com.example.proyectoandroid_soler_martin_perez;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,7 +24,19 @@ public class Buzon_Sugerencias extends AppCompatActivity {
         nombre = findViewById(R.id.editTextTextPersonName);
         button = findViewById(R.id.Enviar);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mandarCorreo();
+            }
+        });
 
+    }
+    private void mandarCorreo(){
+        //Intent email = new Intent(Intent.ACTION_SEND);
+        //email.setData(Uri.parse("mailto: "));
+        //email.setType("text/plain");
+        //email.putExtra(Intent.EXTRA_EMAIL.);
 
     }
 
