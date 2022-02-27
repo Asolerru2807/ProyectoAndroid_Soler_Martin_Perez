e8cbd1cfd713fc07fa77710e1f3db05db86e617b
Mpgcole <mpgcole@gmail.com>
2022-02-27 15:34:48 +0100

    Info de usuario

--- app/src/main/java/com/example/proyectoandroid_soler_martin_perez/Menu.java
+++ app/src/main/java/com/example/proyectoandroid_soler_martin_perez/Menu.java
@@ -3,6 +3,7 @@ package com.example.proyectoandroid_soler_martin_perez;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -18,12 +20,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class Menu extends AppCompatActivity {
 
+    SharedPreferences datos;
+    String [] nombre;
+    String usuario;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-
+        TextView saludo = (TextView) findViewById(R.id.Titulo);
+        TextView info = (TextView) findViewById(R.id.informacion);
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -31,6 +39,14 @@ public class Menu extends AppCompatActivity {
         NavigationView navigation = (NavigationView) findViewById(R.id.navigation);
 
 
+        datos = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        usuario = datos.getString("usuario", "");
+        if((datos.contains("usuario")&&(datos.contains("password")))) {
+            nombre= usuario.split("@");
+            saludo.setText("Bienvenido: "+nombre[0]);
+
+            info.setText("Informaicón de cuenta\n\nNombre: "+nombre[0]+"\n\nCorreo Electronico: "+usuario);
+        }
 
 
         fab.setOnClickListener(new View.OnClickListener() {
